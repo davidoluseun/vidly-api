084a51f6bb9fb63cc319bfddad799e8a67246ade
David Oluseun <a.davidoluseun@gmail.com>
2021-11-21 18:42:08 +0100

    fix: fix cors error

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
+require("./startup/cors")(app);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () =>
