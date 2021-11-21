cfaaa8250e2a34ae257577107b69902b937cee13
David Oluseun <a.davidoluseun@gmail.com>
2021-11-21 19:09:47 +0100

    fix: load cors before routes module

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require("express");
 const app = express();
 
 require("./startup/logging")();
+require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
-require("./startup/cors")(app);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () =>
